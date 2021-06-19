8e7e2120a184460b1acfa3da70049ef181ca70f4
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-19 10:24:35 -0300

    feat: ensure that MongoPlanetRepository returns the Planet on load success

--- src/infra/repositories/mongo-planet-repository.ts
+++ src/infra/repositories/mongo-planet-repository.ts
@@ -1,11 +1,20 @@
-import { AddPlanetRepository, AddPlanetRepositoryParams } from '../../data/protocols'
+import { AddPlanetRepository, AddPlanetRepositoryParams, LoadPlanetByNameRepository } from '../../data/protocols'
 import { PlanetModel } from '../../domain/models'
 import { MongoHelper } from '../helpers'
 
-export class MongoPlanetRepository implements AddPlanetRepository {
+export class MongoPlanetRepository implements AddPlanetRepository, LoadPlanetByNameRepository {
   async add (addPlanetRepositoryParams: AddPlanetRepositoryParams): Promise<PlanetModel> {
     const planetsCollection = await MongoHelper.getCollection('planets')
     const result = await planetsCollection.insertOne(addPlanetRepositoryParams)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByName (name: string): Promise<PlanetModel> {
+    const planetsCollection = await MongoHelper.getCollection('planets')
+    const result = await planetsCollection.findOne({ name })
+    if (result) {
+      return MongoHelper.map(result)
+    }
+    return null
   }
 }
