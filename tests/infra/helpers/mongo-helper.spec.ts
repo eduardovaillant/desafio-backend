6238f8bd95eb64338ae0b94edde00be0e90554d0
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-19 09:13:33 -0300

    test: add MongoHelper

---
+++ tests/infra/helpers/mongo-helper.spec.ts
@@ -0,0 +1,20 @@
+
+import { MongoHelper as sut } from '../../../src/infra/helpers'
+
+describe('MongoHelper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL || 'mongodb://mongo:27017/b2w')
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('should reconnect if mongodb is down', async () => {
+    let accountCollection = await sut.getCollection('planets')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('planets')
+    expect(accountCollection).toBeTruthy()
+  })
+})
