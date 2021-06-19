03858f0e88b6825c34e6c155cc29f563476fd7fe
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-19 09:45:03 -0300

    test: ensure that MongPlanetRepository returns the created planed on add success

---
+++ tests/infra/repositories/mongo-planet-repository.spec.ts
@@ -0,0 +1,36 @@
+import { MongoHelper } from '../../../src/infra/helpers'
+import { MongoPlanetRepository } from '../../../src/infra/repositories/mongo-planet-repository'
+import { mockAddPlanetRepositoryParams } from '../../data/mocks/repositories'
+
+import { Collection } from 'mongodb'
+
+let planetCollection: Collection
+
+const makeSut = (): MongoPlanetRepository => {
+  return new MongoPlanetRepository()
+}
+
+describe('MongoPlanetRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    planetCollection = await MongoHelper.getCollection('planets')
+    await planetCollection.deleteMany({})
+  })
+
+  describe('add()', () => {
+    test('should return the created planet on success ', async () => {
+      const sut = makeSut()
+      const addPlanetRepositoryParams = mockAddPlanetRepositoryParams()
+      const planet = await sut.add(addPlanetRepositoryParams)
+      expect(planet.id).toBeTruthy()
+      expect(planet.name).toBe('any_name')
+    })
+  })
+})
