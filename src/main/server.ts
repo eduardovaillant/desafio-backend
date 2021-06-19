25e8b0828c2c4c6c80c4ece7e577a320670dd646
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-19 11:32:29 -0300

    chore: add tsconfigbuild

---
+++ src/main/server.ts
@@ -0,0 +1,9 @@
+import env from '../main/config/env'
+import { MongoHelper } from '../infra/helpers'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
