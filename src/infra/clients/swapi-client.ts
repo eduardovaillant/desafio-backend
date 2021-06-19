a2941e2959ce094413b8697e8a3ba83ad681e1e3
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-19 16:26:46 -0300

    feat: ensure that SwapiClient calls the api with correct values

--- src/infra/clients/swapi-client.ts
+++ src/infra/clients/swapi-client.ts
@@ -6,15 +6,16 @@ export class SwapiClientImpl implements SwapiClient {
   async search (name: string): Promise<SwapiPlanetReturn> {
     const url = `https://swapi.dev/api/planets/?search=${name}`
 
-    const response = await axios.get(url)
+    await axios.get(url)
 
-    const swapiPlanetReturn: SwapiPlanetReturn = {
-      name: response.data.results[0].name,
-      climate: response.data.results[0].climate,
-      terrain: response.data.results[0].terrain,
-      movie_apparitions: response.data.results[0].films.length
-    }
+    // const swapiPlanetReturn: SwapiPlanetReturn = {
+    //   name: response.data.results[0].name,
+    //   climate: response.data.results[0].climate,
+    //   terrain: response.data.results[0].terrain,
+    //   movie_apparitions: response.data.results[0].films.length
+    // }
 
-    return swapiPlanetReturn
+    // return swapiPlanetReturn
+    return null
   }
 }
