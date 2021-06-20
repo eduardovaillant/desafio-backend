3491d2c8d5f55aea9869bd666e51310d056d519c
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-20 14:35:44 -0300

    feat: add RemovePlanetController

--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './add-planet-controller'
 export * from './load-planet-by-name-controller'
+export * from './remove-planet-controller'
---
+++ src/presentation/controllers/remove-planet-controller.ts
@@ -0,0 +1,32 @@
+import { RemovePlanet } from '../../domain/usecases'
+import { ok, serverError } from '../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../protocols'
+
+export class RemovePlanetController implements Controller {
+  constructor (
+    private readonly removePlanet: RemovePlanet
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const id = httpRequest.query.id
+
+      const removed = await this.removePlanet.remove(id)
+
+      const result = {
+        deleted: 0,
+        not_deleted: 1
+      }
+
+      if (removed) {
+        result.deleted = 1
+        result.not_deleted = 0
+      }
+
+      return ok(result)
+    } catch (error) {
+      console.error(error)
+      return serverError(error)
+    }
+  }
+}
