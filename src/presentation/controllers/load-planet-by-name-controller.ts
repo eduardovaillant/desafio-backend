6dd4acfebad9ab5fcab47cac02f1baeb9a9d33cc
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-20 08:35:51 -0300

    feat: ensure that LoadPlanetByNameController calls LoadPlanetById wit correct value

--- src/presentation/controllers/load-planet-by-name-controller.ts
+++ src/presentation/controllers/load-planet-by-name-controller.ts
@@ -1,17 +1,28 @@
-import { LoadPlanetByName } from '../../domain/usecases'
+import { PlanetModel } from 'domain/models'
+import { LoadPlanetById, LoadPlanetByName } from '../../domain/usecases'
 import { ok, serverError } from '../helpers'
 import { Controller, HttpRequest, HttpResponse } from '../protocols'
 
 export class LoadPlanetByNameController implements Controller {
   constructor (
-    private readonly loadPlanetByName: LoadPlanetByName
+    private readonly loadPlanetByName: LoadPlanetByName,
+    private readonly loadPlanetById: LoadPlanetById
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      // TODO validate the name?
-      const name = httpRequest.query.name
-      const planet = await this.loadPlanetByName.loadByName(name)
+      // TODO pensar em uma forma melhor para organizar isso
+      const { name, id } = httpRequest.query
+      let planet: PlanetModel
+
+      if (name) {
+        planet = await this.loadPlanetByName.loadByName(name)
+      }
+
+      if (id) {
+        planet = await this.loadPlanetById.loadById(id)
+      }
+
       return ok(planet)
     } catch (error) {
       console.error(error)
--- tests/domain/usecases/load-planet-by-name.ts
+++ tests/domain/usecases/load-planet-by-name.ts
@@ -1,6 +1,6 @@
 import { PlanetModel } from '../../../src/domain/models'
 import { mockPlanetModel } from '../mocks/planet'
-import { LoadPlanetByName } from '.'
+import { LoadPlanetById, LoadPlanetByName } from '.'
 
 export class LoadPlanetByNameSpy implements LoadPlanetByName {
   name: string
@@ -8,6 +8,16 @@ export class LoadPlanetByNameSpy implements LoadPlanetByName {
 
   async loadByName (name: string): Promise<PlanetModel> {
     this.name = name
+    return this.planetModel
+  }
+}
+
+export class LoadPlanetByIdSpy implements LoadPlanetById {
+  id: string
+  planetModel: PlanetModel = mockPlanetModel()
+
+  async loadById (id: string): Promise<PlanetModel> {
+    this.id = id
     return this.planetModel
   }
 }
