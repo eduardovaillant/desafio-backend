5d85ff61ecd6a3a36507d9ccd64ae11c6d1932d1
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-06-21 17:49:19 -0300

    chore: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/b2w',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/desafio-backend',
   port: process.env.PORT || 3000,
   baseUrl: process.env.BASE_URL || 'localhost:3000/api/planets/'
 }
